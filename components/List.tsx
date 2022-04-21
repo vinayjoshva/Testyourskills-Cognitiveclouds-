eae2f399de68c11d210e3fca3000e91cfef02846
vinayjoshva <91328195+vinayjoshva@users.noreply.github.com>
2022-04-21 20:44:50 +0530

    Add files via upload

---
+++ components/List.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './List.css';
+
+const listOfItems = [
+  {
+    title: "Learn React",
+    url: "https://eprint.iacr.org/2021/1022",
+    createdAt: "2011-12-12",
+    num_comments: 12,
+    objectId: 1,
+  },
+  {
+    title: "Learn TypeScript",
+    url: "https://eprint.iacr.org/2021/1022",
+    createdAt: "2012-12-12",
+    num_comments: 1,
+    objectId: 2,
+  },
+]
+const List = () => {
+  return (
+    <div>
+      <ul>
+        <li>{listOfItems[0].title}</li>
+        <li>{listOfItems[0].url}</li>
+        <li>{listOfItems[0].createdAt}</li>
+        <li>{listOfItems[0].num_comments}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default List;
