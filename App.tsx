9cd1b059a8ad36b5eb8f880985735f3d38b2c813
vinayjoshva <91328195+vinayjoshva@users.noreply.github.com>
2022-04-22 12:26:02 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+import logo from './asset/logo.png';
+import List from './components/List';
+import Search from './components/Search';
+
+// const listOfItems = [
+//   {
+//     title: "Learn React",
+//     url: "https://eprint.iacr.org/2021/1022",
+//     created_at: "2011-12-12",
+//     author: "grey-area",
+//     points: 1107,
+//     num_comments: 12,
+//     objectID: 1,
+//   },
+//   {
+//     title: "Learn TypeScript",
+//     url: "https://eprint.iacr.org/2021/1022",
+//     created_at: "2012-12-12",
+//     author: "grey-area",
+//     points: 6107,
+//     num_comments: 1,
+//     objectID: 2,
+//   },
+//   {
+//     created_at: "2017-02-19T21:16:33.000Z",
+//     title: "Reflecting on one very, very strange year at Uber",
+//     url: "https://www.susanjfowler.com/blog/2017/2/19/reflecting-on-one-very-strange-year-at-uber",
+//     author: "grey-area",
+//     points: 4107,
+//     num_comments: 530,
+//     objectID: 3,
+//   },
+//   {
+//     created_at: "2021-04-05T14:04:22.000Z",
+//     title: "Google’s copying of the Java SE API was fair use [pdf]",
+//     url: "https://www.supremecourt.gov/opinions/20pdf/18-956_d18f.pdf",
+//     author: "pdoconnell",
+//     points: 4103,
+//     num_comments: 930,
+//     objectID: 4,
+//   },
+// ];
+
+function App() {
+  const title = 'React training';
+  return (
+    <div>
+      <nav>
+        <div className="heading">
+          <h1>{title}</h1>
+          <img src={logo}/>
+        </div>
+        <Search/>
+        </nav>
+        <List/>
+    </div>
+  );
+}
+
+export default App;
