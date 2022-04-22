decc6d347db15ddec149b027e709d5ff6cfcac1b
vinayjoshva <91328195+vinayjoshva@users.noreply.github.com>
2022-04-22 12:27:27 +0530

    Add files via upload

---
+++ components/Search.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Search.css';
+
+const Search = () => {
+  return (
+    <div>
+      <label htmlFor="searchBox">Search</label>
+      <input type="text" id="searchBox"/>
+    </div>
+  );
+};
+
+export default Search;
