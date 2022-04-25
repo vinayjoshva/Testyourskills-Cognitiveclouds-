35663c9a381fda9cf4bf19ad3acff60b140de08f
vinayjoshva <91328195+vinayjoshva@users.noreply.github.com>
2022-04-25 22:06:28 +0530

    Add files via upload

---
+++ src/components/List.tsx
@@ -0,0 +1,37 @@
+import "./List.css";
+import Item from "./Item";
+import { StoryType } from "../types";
+
+type ListProps = {
+  listOfItems: Array<StoryType>;
+  onClickDelete: (e: number) => void;
+};
+
+const List = ({ listOfItems, onClickDelete }: ListProps) => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>URL</th>
+            <th>Author</th>
+            <th>Comments</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listOfItems.map((item) => (
+            <Item
+              key={item.objectID}
+              item={item}
+              onClickDelete={onClickDelete}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default List;
